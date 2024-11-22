e435a5449cd1f8bf0d51d8a6e69cefe7efc7f701
amarkos27 <armarkos27@gmail.com>
2024-11-22 09:34:20 -0500

    Add font and styling touches

--- src/app.js
+++ src/app.js
@@ -1,10 +1,2 @@
 import 'normalize.css';
 import './style.css';
-
-const content = document.querySelector('.content');
-document.getElementById('ten-day').addEventListener('click', movePage);
-document.getElementById('return').addEventListener('click', movePage);
-
-function movePage() {
-  content.classList.toggle('pageTwo');
-}
---
+++ src/display.js
@@ -0,0 +1,5 @@
+const Display = (() => {
+  const header = document.querySelector('.main header');
+  const hourlyConditions = document.querySelector('.hourly-conditions');
+  const loadingIcon = document.getElementById('.loading');
+})();
